f0ddac105f90df96388a10e8e3da664b8cc41b48
Olha Kabatska <olga.kabatskaya@ubn24.de>
2025-09-01 01:40:49 +0200

    feat: add image to not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,18 @@
 import Link from "@/components/ui/Link";
+import Image from "next/image";
 
 export default function NotFound() {
   return (
     <>
       <div className="flex flex-col gap-3">
+        <Image
+          className="dark:invert dark:brightness-0 dark:contrast-100 w-96 h-40"
+          src="/logo.svg"
+          alt="Doppentsolecker logo"
+          width={400}
+          height={160}
+          priority
+        />
         <h1 className=" typography-h3">Nicht gefunden</h1>
         <p>Die angeforderte Ressource wurde nicht gefunden.</p>
       </div>
