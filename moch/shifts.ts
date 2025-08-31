b7e4da8052516cd1c428af04686deb9626733a10
Olha Kabatska <olga.kabatskaya@ubn24.de>
2025-08-31 23:11:05 +0200

    chore: add demo shifts data feat: add service for fetching shifts

---
+++ moch/shifts.ts
@@ -0,0 +1,41 @@
+import { Shift } from "@/lib/features/shifts/types";
+
+export const demoShifts: Shift[] = [
+  {
+    id: "s1",
+    employeeId: "e1",
+    date: "2025-09-01",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s2",
+    employeeId: "e1",
+    date: "2025-09-01",
+    start: "17:00",
+    end: "21:30",
+  },
+
+  {
+    id: "s3",
+    employeeId: "e2",
+    date: "2025-09-01",
+    start: "12:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s4",
+    employeeId: "e3",
+    date: "2025-09-02",
+    start: "10:30",
+    end: "15:00",
+  },
+  {
+    id: "s5",
+    employeeId: "e3",
+    date: "2025-09-02",
+    start: "18:00",
+    end: "22:00",
+  },
+];
---
+++ services/shifts.ts
@@ -0,0 +1,5 @@
+import { demoShifts } from "@/moch/shifts";
+
+export async function fetchShifts() {
+  return demoShifts;
+}
