f1f16a910b788ad3474271162ecded9c43a9b937
Olha Kabatska <olga.kabatskaya@ubn24.de>
2025-08-31 21:12:36 +0200

    chore: remove unused demo content refactor: extract footer into separate component

---
+++ components/layout/footer.tsx
@@ -0,0 +1,15 @@
+import { footerLinks } from "@/config/navigation";
+import Link from "@/components/ui/Link";
+
+const Footer = () => {
+  return (
+    <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      Danke für Ihre Besuch!
+      {footerLinks.map((l, k) => (
+        <Link key={k} link={l} />
+      ))}
+    </footer>
+  );
+};
+
+export default Footer;
