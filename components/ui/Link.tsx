5a287bebe2c4b683efa8aa164221121de4cbb02f
Olha Kabatska <olga.kabatskaya@ubn24.de>
2025-08-31 21:10:50 +0200

    feat: add Link ui component

---
+++ components/ui/Link.tsx
@@ -0,0 +1,20 @@
+import type { Link as LinkType } from "@/config/navigation";
+import Image from "next/image";
+
+const Link = ({ link: { href, title, img, target } }: { link: LinkType }) => {
+  return (
+    <a
+      className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+      href={href}
+      target={target}
+      rel="noopener noreferrer"
+    >
+      {img && (
+        <Image aria-hidden src={img} alt="File icon" width={16} height={16} />
+      )}
+      {title}
+    </a>
+  );
+};
+
+export default Link;
