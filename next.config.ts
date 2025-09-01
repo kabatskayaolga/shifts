48b82f3c8937246fc1373ca617b00abe3df2b514
Olha Kabatska <olga.kabatskaya@ubn24.de>
2025-09-01 06:58:51 +0200

    chore: deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/shifts",
+  assetPrefix: "/shifts/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
