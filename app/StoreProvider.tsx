430f70436505532686bfc6c58b5edf7f2d6843ac
Olha Kabatska <olga.kabatskaya@ubn24.de>
2025-09-01 02:14:19 +0200

    fix: add important code to StoreProvider

--- app/StoreProvider.tsx
+++ app/StoreProvider.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { Provider } from "react-redux";
 import { makeStore, AppStore } from "../lib/store";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export default function StoreProvider({
   children,
@@ -12,6 +13,13 @@ export default function StoreProvider({
   if (!storeRef.current) {
     storeRef.current = makeStore();
   }
+
+  useEffect(() => {
+    if (storeRef.current != null) {
+      const unsubscribe = setupListeners(storeRef.current.dispatch);
+      return unsubscribe;
+    }
+  }, []);
 
   return <Provider store={storeRef.current}>{children}</Provider>;
 }
