aeaad8980a086b2fc61882c4adef2cb1f4d752a1
Olha Kabatska <olga.kabatskaya@ubn24.de>
2025-09-01 02:46:15 +0200

    chore: add more demo data

--- moch/employees.ts
+++ moch/employees.ts
@@ -4,4 +4,9 @@ export const demoEmployees: Employee[] = [
   { id: "e1", name: "Anna M.", contract: "fullTime" },
   { id: "e2", name: "Jonas K.", contract: "partTime" },
   { id: "e3", name: "Mira S.", contract: "miniJob" },
+  { id: "e4", name: "Lena P.", contract: "partTime" },
+  { id: "e5", name: "Max H.", contract: "fullTime" },
+  { id: "e6", name: "Sara T.", contract: "miniJob" },
+  { id: "e7", name: "Tom B.", contract: "partTime" },
+  { id: "e8", name: "Emily R.", contract: "fullTime" },
 ];
--- moch/shifts.ts
+++ moch/shifts.ts
@@ -10,32 +10,356 @@ export const demoShifts: Shift[] = [
   },
   {
     id: "s2",
-    employeeId: "e1",
-    date: "2025-09-01",
-    start: "17:00",
-    end: "21:30",
-  },
-
-  {
-    id: "s3",
     employeeId: "e2",
     date: "2025-09-01",
-    start: "12:00",
+    start: "14:00",
     end: "18:00",
   },
 
   {
-    id: "s4",
+    id: "s3",
     employeeId: "e3",
     date: "2025-09-02",
-    start: "10:30",
-    end: "15:00",
+    start: "09:00",
+    end: "13:00",
   },
   {
-    id: "s5",
-    employeeId: "e3",
+    id: "s4",
+    employeeId: "e1",
     date: "2025-09-02",
-    start: "18:00",
-    end: "22:00",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s5",
+    employeeId: "e2",
+    date: "2025-09-03",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s6",
+    employeeId: "e3",
+    date: "2025-09-03",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s7",
+    employeeId: "e1",
+    date: "2025-09-04",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s8",
+    employeeId: "e2",
+    date: "2025-09-04",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s9",
+    employeeId: "e3",
+    date: "2025-09-05",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s10",
+    employeeId: "e1",
+    date: "2025-09-05",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s11",
+    employeeId: "e2",
+    date: "2025-09-06",
+    start: "10:00",
+    end: "15:00",
+  },
+
+  {
+    id: "s12",
+    employeeId: "e3",
+    date: "2025-09-08",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s13",
+    employeeId: "e1",
+    date: "2025-09-08",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s14",
+    employeeId: "e2",
+    date: "2025-09-09",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s15",
+    employeeId: "e3",
+    date: "2025-09-09",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s16",
+    employeeId: "e1",
+    date: "2025-09-10",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s17",
+    employeeId: "e2",
+    date: "2025-09-10",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s18",
+    employeeId: "e3",
+    date: "2025-09-11",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s19",
+    employeeId: "e1",
+    date: "2025-09-11",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s20",
+    employeeId: "e2",
+    date: "2025-09-12",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s21",
+    employeeId: "e3",
+    date: "2025-09-12",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s22",
+    employeeId: "e1",
+    date: "2025-09-13",
+    start: "10:00",
+    end: "15:00",
+  },
+
+  {
+    id: "s23",
+    employeeId: "e2",
+    date: "2025-09-15",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s24",
+    employeeId: "e3",
+    date: "2025-09-15",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s25",
+    employeeId: "e1",
+    date: "2025-09-16",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s26",
+    employeeId: "e2",
+    date: "2025-09-16",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s27",
+    employeeId: "e3",
+    date: "2025-09-17",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s28",
+    employeeId: "e1",
+    date: "2025-09-17",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s29",
+    employeeId: "e2",
+    date: "2025-09-18",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s30",
+    employeeId: "e3",
+    date: "2025-09-18",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s31",
+    employeeId: "e1",
+    date: "2025-09-19",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s32",
+    employeeId: "e2",
+    date: "2025-09-19",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s33",
+    employeeId: "e3",
+    date: "2025-09-20",
+    start: "10:00",
+    end: "15:00",
+  },
+
+  {
+    id: "s34",
+    employeeId: "e1",
+    date: "2025-09-22",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s35",
+    employeeId: "e2",
+    date: "2025-09-22",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s36",
+    employeeId: "e3",
+    date: "2025-09-23",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s37",
+    employeeId: "e1",
+    date: "2025-09-23",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s38",
+    employeeId: "e2",
+    date: "2025-09-24",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s39",
+    employeeId: "e3",
+    date: "2025-09-24",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s40",
+    employeeId: "e1",
+    date: "2025-09-25",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s41",
+    employeeId: "e2",
+    date: "2025-09-25",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s42",
+    employeeId: "e3",
+    date: "2025-09-26",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s43",
+    employeeId: "e1",
+    date: "2025-09-26",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s44",
+    employeeId: "e2",
+    date: "2025-09-27",
+    start: "10:00",
+    end: "15:00",
+  },
+
+  {
+    id: "s45",
+    employeeId: "e3",
+    date: "2025-09-29",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s46",
+    employeeId: "e1",
+    date: "2025-09-29",
+    start: "14:00",
+    end: "18:00",
+  },
+
+  {
+    id: "s47",
+    employeeId: "e2",
+    date: "2025-09-30",
+    start: "09:00",
+    end: "13:00",
+  },
+  {
+    id: "s48",
+    employeeId: "e3",
+    date: "2025-09-30",
+    start: "14:00",
+    end: "18:00",
   },
 ];
