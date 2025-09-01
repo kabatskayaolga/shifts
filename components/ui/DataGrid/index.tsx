54ba1e4d924527384a07a543d27551ae42991391
Olha Kabatska <olga.kabatskaya@ubn24.de>
2025-09-01 08:20:15 +0200

    style: rewrite table styles

--- components/ui/DataGrid/index.tsx
+++ components/ui/DataGrid/index.tsx
@@ -1,59 +1,61 @@
 "use client";
 import { DataGrid as MuiDataGrid, GridValidRowModel } from "@mui/x-data-grid";
-import { Box, createTheme, ThemeProvider } from "@mui/material";
+import { Box } from "@mui/material";
 import React from "react";
-import { useTheme } from "next-themes";
 
 type Props<T extends GridValidRowModel> = React.ComponentProps<
   typeof MuiDataGrid<T>
 >;
 
-const getTheme = () =>
-  createTheme({
-    components: {
-      MuiDataGrid: {
-        styleOverrides: {
-          root: {
-            backgroundColor: "var(--background)",
-            color: "var(--foreground)",
-            border: "none",
-            "--DataGrid-bgcolor": "var(--background)",
-            "--DataGrid-headerBackground": "var(--background)",
-            "--DataGrid-pinnedBackground": "var(--background)",
-
-            "& .MuiDataGrid-cell": {
-              borderBottom: "none",
-            },
-            "& .MuiDataGrid-columnHeaders": {
-              borderBottom: "none",
-              backgroundColor: "var(--background)",
-            },
-
-            "& .MuiDataGrid-withBorderColor": {
-              backgroundColor: "var(--background)",
-              borderColor: "transparent",
-            },
-          },
-        },
-      },
-    },
-  });
-
 export default function DataGrid<T extends GridValidRowModel>(props: Props<T>) {
-  const { theme } = useTheme();
-  const themeClass = React.useMemo(() => theme && getTheme(), [theme]);
-  if (!themeClass) return;
   return (
     <Box sx={{ width: "100%", height: "100%" }}>
-      <ThemeProvider theme={themeClass}>
-        <MuiDataGrid
-          className=" w-full"
-          {...props}
-          rowHeight={34}
-          disableColumnFilter
-          hideFooter
-        />
-      </ThemeProvider>
+      <MuiDataGrid
+        className=" w-full"
+        {...props}
+        rowHeight={34}
+        disableColumnFilter
+        hideFooter
+        sx={{
+          border: 0,
+          backgroundColor: "var(--background)",
+          color: "var(--foreground)",
+          "& .MuiDataGrid-columnHeaders .MuiDataGrid-filler": {
+            backgroundColor: "var(--background)",
+          },
+          "& .MuiDataGrid-columnHeader": {
+            backgroundColor: "var(--background)",
+            color: "var(--background)",
+            // "&:hover": {
+            //   backgroundColor: "#1e1e1e",
+            //   cursor: "pointer",
+            // },
+            "& .MuiDataGrid-filler": {
+              backgroundColor: "#d0d0d021",
+            },
+            "& .MuiDataGrid-scrollbarFiller": {
+              backgroundColor: "var(--background)",
+            },
+          },
+          "& .MuiDataGrid-row": {
+            "--rowBorderColor": "#d0d0d021",
+            "&:hover": { backgroundColor: "#d0d0d021" },
+          },
+          "& .MuiDataGrid-row.Mui-selected": {
+            backgroundColor: "#d0d0d021",
+            "&:hover": { backgroundColor: "#d0d0d021" },
+          },
+
+          "& .MuiDataGrid-pinnedColumnHeaders": {
+            backgroundColor: "var(--background)",
+            color: "var(--foreground)",
+          },
+          "& .MuiDataGrid-columnHeaderTitle": {
+            color: "var(--foreground)",
+            fontWeight: 600,
+          },
+        }}
+      />
     </Box>
   );
 }
