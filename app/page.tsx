b4c95ff98777b73b9268dddfe5f9833d5c3456ec
Olha Kabatska <olga.kabatskaya@ubn24.de>
2025-09-01 23:18:46 +0200

    chore: update home pageheader

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-8 w-full items-center">
       <h1 className="text-3xl sm:text-4xl md:text-6xl">
-        Dienstpläne für September
+        Dienstplan für September
       </h1>
       <Schifts />
     </div>
