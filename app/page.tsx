1a646b0475cafd4f8298fc8ba0452eea2890b235
Olha Kabatska <olga.kabatskaya@ubn24.de>
2025-09-01 02:14:49 +0200

    refactor: remove image from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
 import Schifts from "./shifts/Schifts";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div className="flex items-center gap-8 flex-wrap">
-        <Image
-          className="dark:invert dark:brightness-0 dark:contrast-100 w-80 h-24"
-          src="/logo.svg"
-          alt="Doppentsolecker logo"
-          width={320}
-          height={96}
-          priority
-        />
-        <h1 className=" typography-h3">Dienstpläne für September</h1>
-      </div>
+      <h1 className=" typography-h3">Dienstpläne für September</h1>
       <Schifts />
     </>
   );
